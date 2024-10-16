17fb47d36d135078c55c8d49381d2a1565be142f
ppclear <36061236+rookie-web@users.noreply.github.com>
2024-10-16 23:09:36 +0800

    新建下拉分页选择路由

--- docs/config/sidebar.js
+++ docs/config/sidebar.js
@@ -5,85 +5,90 @@
 //   },
 // ];
 
-
 export default {
   '/code-style/': [
     {
       text: '风格规范',
       items: [
-        { text: "快速上手", link: "/code-style/quick-start/" },
-        { text: "项目文件夹", link: "/code-style/file/" },
-        { text: "编程规范约定", link: "/code-style/code/" },
-        { text: "template模板", link: "/code-style/template/" },
-        { text: "typescript模板", link: "/code-style/typescript/" },
-        { text: "type类型声明", link: "/code-style/type/" },
-        { text: "style样式", link: "/code-style/style/" },        
+        { text: '快速上手', link: '/code-style/quick-start/' },
+        { text: '项目文件夹', link: '/code-style/file/' },
+        { text: '编程规范约定', link: '/code-style/code/' },
+        { text: 'template模板', link: '/code-style/template/' },
+        { text: 'typescript模板', link: '/code-style/typescript/' },
+        { text: 'type类型声明', link: '/code-style/type/' },
+        { text: 'style样式', link: '/code-style/style/' }
         // { text: "color颜色", link: "/code-style/color/" },
         // { text: "border边框字体", link: "/code-style/border/" },
-      ],
+      ]
     }
   ],
   '/interview/': [
     {
       text: 'HTML',
-      link: "/interview/html/",
+      link: '/interview/html/'
     },
     {
       text: 'CSS',
-      link: "/interview/css/",
+      link: '/interview/css/'
     },
     {
       text: 'JS基础',
-      link: "/interview/js/",
+      link: '/interview/js/'
     },
     {
       text: 'ES6+',
-      link: "/interview/es6/",
+      link: '/interview/es6/'
     },
     {
       text: 'Vue2',
-      link: "/interview/vue2/",
+      link: '/interview/vue2/'
     },
     {
       text: 'Vue3',
-      link: "/interview/vue3/",
+      link: '/interview/vue3/'
     },
     {
       text: '计算机网络',
-      link: "/interview/network/",
+      link: '/interview/network/'
     },
     {
       text: '浏览器相关',
-      items:[
-        { text: "大纲", link: "/interview/browser/" },
-        { text: "事件循环", link: "/interview/browser/eventloop" },
+      items: [
+        { text: '大纲', link: '/interview/browser/' },
+        { text: '事件循环', link: '/interview/browser/eventloop' }
       ]
     },
     {
       text: '前端性能优化',
-      link: "/interview/optimize/",
+      link: '/interview/optimize/'
     },
     {
       text: '手写实现',
-      link: "/interview/handwriting/",
+      link: '/interview/handwriting/'
     },
     {
       text: '工程化',
-      items:[
-        { text: "webpack", link: "/interview/webpack/" },
-        { text: "vite", link: "/interview/vite/" },
+      items: [
+        { text: 'webpack', link: '/interview/webpack/' },
+        { text: 'vite', link: '/interview/vite/' }
       ]
-    },
+    }
   ],
-  '/best-practice/':[
+  '/best-practice/': [
     {
       text: 'webp格式图片',
-      link: "/best-practice/webp-photo/",
+      link: '/best-practice/webp-photo/'
     },
     {
       text: '类的继承(ES5)',
-      link: "/best-practice/class-extends/",
-    },
+      link: '/best-practice/class-extends/'
+    }
   ],
-  '/': [],
+  '/common-components/': [
+    {
+      text: '下拉分页选择',
+      link: '/common-components/select-pager/'
+    }
+  ],
+  '/': []
 }
