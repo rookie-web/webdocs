ef79df95ae289cb5274476ab1c625f006f54ed1e
caojs <1164778423@qq.com>
2024-10-16 22:47:13 +0800

    格式化配置

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -3,7 +3,7 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import './style.css'
 
-export default  {
+export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
     // 注册自定义全局组件
